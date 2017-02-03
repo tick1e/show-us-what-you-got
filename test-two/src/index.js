51fc8b53f354cf6a7314c9f292ee91521cef8983
tick1e <matt.tick13@gmail.com>
2017-02-03 12:58:12 +1100

    Add files via upload

--- test-two/src/index.js
+++ test-two/src/index.js
@@ -4,20 +4,24 @@ import Http from './helpers/http';
 
 const baseUrl = "https://api.github.com/";
 const organisationId = "uber";
-const gitHubApiAuthToken = "fa9291db784c97e77e68b85f9a74caf85bd3606d"; //add your GitHub API OAuth key here to increase request limit
+const gitHubApiAuthToken = ""; //add your GitHub API OAuth key here to increase request limit
 
 let logger = new Logger();
 let http = new Http();
 
 let gitHubService = new GitHubService(baseUrl, http, gitHubApiAuthToken);
 
+logger.log(gitHubService.getUsersForOrganisation(organisationId));
+
 gitHubService.getUsersForOrganisation(organisationId).then((users) => {
     users.forEach((user) => {
         logger.log("Username: " + user.login);
 		
-		users.forEach((user) => {
-			logger.log("Repos: " + user.login.repos);
-			});
+		gitHubService.getUser(user.login).then((repos) => {
+			repos.forEach((repo) => {
+			logger.log("Repos: " + repos.name);
+		})
+		})		
     });
 }).catch((error) => {
     logger.log("Error: " + error);
