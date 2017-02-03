6c3a97ae8964122ccf5233488fc09a4257d7713f
tick1e <matt.tick13@gmail.com>
2017-02-04 03:01:45 +1100

    Add files via upload

--- test-two/src/index.js
+++ test-two/src/index.js
@@ -1,29 +1,39 @@
 import Logger from './helpers/logger';
 import GitHubService from './services/github';
 import Http from './helpers/http';
+require('dotenv').config();
+
+var db = require('db')
+db.connect({
+  key: process.env.DB_KEY,
+})
+
 
 const baseUrl = "https://api.github.com/";
 const organisationId = "uber";
-const gitHubApiAuthToken = ""; //add your GitHub API OAuth key here to increase request limit
+const gitHubApiAuthToken = key; //add your GitHub API OAuth key here to increase request limit
 
 let logger = new Logger();
 let http = new Http();
 
+
 let gitHubService = new GitHubService(baseUrl, http, gitHubApiAuthToken);
 
-logger.log(gitHubService.getUsersForOrganisation(organisationId));
 
 gitHubService.getUsersForOrganisation(organisationId).then((users) => {
     users.forEach((user) => {
-        logger.log("Username: " + user.login);
-		
-		gitHubService.getUser(user.login).then((repos) => {
-			repos.forEach((repo) => {
-			logger.log("Repos: " + repos.name);
-		})
-		})		
+		logger.log(user.login);
+			gitHubService.getUserRepos(user.login).then((repos) => {
+				repos.forEach((repo) => {
+					
+				logger.log(user.login + "has worked on: " + repo.name);
+				})
+			})		
     });
 }).catch((error) => {
     logger.log("Error: " + error);
 });
+
+
+
 
